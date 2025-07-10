18181de28a482463a767a8d1196c496f21a4ac3a
Aditya Sinha <sinha.aditya.btech2024@sitpune.edu.in>
2025-07-10 00:21:19 -0400

    Create Positive_or_negative.cpp
    Experiment 3a

---
+++ Positive_or_negative.cpp
@@ -0,0 +1,23 @@
+// Name - Aditya Ishan Sinha 
+// PRN - 24070123006
+// Batch - EnTC - A1 
+// Expt - 3a
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    float a;
+    cout << "Please enter a number : ";
+    cin >> a;
+    if (a > 0){
+        cout <<"The number "<< a <<" is positive";
+    }else if(a <0){
+        cout << "The number "<< a << " is negative";
+    }else{
+        cout << "The number is zero. ";
+    }
+    
+    return 0;
+}
